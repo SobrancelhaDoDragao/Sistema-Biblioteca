073de230c863cac345edaa6f52cbb0ecc4446fbe
SobrancelhaDoDragao <eudson.duraes@gmail.com>
2023-03-23 23:10:55 -0300

    Melhorando

--- ApiBiblioteca/Api/models.py
+++ ApiBiblioteca/Api/models.py
@@ -1,36 +1,77 @@
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 from django.db import models
+from django.contrib.auth.models import (
+    BaseUserManager, AbstractBaseUser
+)
 
-class CustomUserManager(BaseUserManager):
-    def create_user(self, email, password=None):
+
+class MyUserManager(BaseUserManager):
+    def create_user(self, email, nome, password=None):
+        """
+        Creates and saves a User with the given email, date of
+        birth and password.
+        """
         if not email:
-            raise ValueError('Usuários devem ter um endereço de e-mail válido')
+            raise ValueError('Users must have an email address')
+
         user = self.model(
             email=self.normalize_email(email),
+             nome=nome,
         )
+
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, email, password):
-        user = self.create_user(email, password=password)
-        user.is_staff = True
-        user.is_superuser = True
+    def create_superuser(self, email, nome, password=None):
+        """
+        Creates and saves a superuser with the given email, date of
+        birth and password.
+        """
+        user = self.create_user(
+            email,
+            password=password,
+            nome=nome,
+        )
+        user.is_admin = True
         user.save(using=self._db)
         return user
 
 
-class CustomUser(AbstractBaseUser, PermissionsMixin):
-    email = models.EmailField(unique=True)
-    nome = models.CharField(max_length=100, blank=True)
-    foto = models.CharField(max_length=100, blank=True)
+class CustomUser(AbstractBaseUser):
+    email = models.EmailField(
+        verbose_name='email address',
+        max_length=255,
+        unique=True,
+    )
+    nome = models.CharField(max_length=50)
+    foto = models.CharField(max_length=100)
+
+    
     is_active = models.BooleanField(default=True)
-    is_staff = models.BooleanField(default=False)
+    is_admin = models.BooleanField(default=False)
+
+    objects = MyUserManager()
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = []
-
-    objects = CustomUserManager()
+    REQUIRED_FIELDS = ['nome']
 
     def __str__(self):
         return self.email
+
+    def has_perm(self, perm, obj=None):
+        "Does the user have a specific permission?"
+        # Simplest possible answer: Yes, always
+        return True
+
+    def has_module_perms(self, app_label):
+        "Does the user have permissions to view the app `app_label`?"
+        # Simplest possible answer: Yes, always
+        return True
+    
+    @property
+    def is_staff(self):
+        "Is the user a member of staff?"
+        # Simplest possible answer: All admins are staff
+        return self.is_admin
+
+ 
