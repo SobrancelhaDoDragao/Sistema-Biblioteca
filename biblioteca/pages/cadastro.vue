af6530b4403b38e6afc72df2dd4417ba9ebbbb28
SobrancelhaDoDragao <eudson.duraes@gmail.com>
2023-03-24 15:59:22 -0300

    Adicionando projeto Nuxt

---
+++ biblioteca/pages/cadastro.vue
@@ -0,0 +1,147 @@
+<template>
+
+    <div id="conteiner">
+
+  
+        <form action="">
+            
+            <h1>Cadastro</h1>
+
+           <label for="nome">Nome</label>
+           <input type="text" name="" id="nome">
+
+           <label for="email">Email</label>
+           <input type="email" name="" id="email">
+
+           <label for="senha">Senha</label>
+           <input type="password" name="" id="senha">
+
+           <label for="senha">Repetir senha</label>
+           <input type="password" name="" id="senha">
+
+           <label for="tipoConta">Criar conta como</label>
+
+            <select name="TipoConta" id="TipoConta">
+
+                <option value="usuario">Usuário</option>
+                <option value="funcionario">Funcionário</option>
+
+            </select>
+
+
+            <button>Cadastrar</button>
+
+        </form>
+    </div>
+
+</template>
+
+<style>
+
+* {
+    box-sizing: border-box;
+    padding: 0;
+    margin: 0;
+    font-family: var(--main-font);
+    font-weight: var(--regular-weight);
+}
+
+#conteiner{
+   height: 100vh;
+   background: var(--colorOne);
+   display: flex;
+   justify-content: center;
+   align-items: center; 
+}
+
+#conteiner form{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 50%;
+}
+
+form h1{
+    color: var(--colorThree);
+    font-weight: var(--bold-weight);
+    font-size: 5rem;
+    text-align: center;
+    width: 100%;
+    letter-spacing: 4px;
+}
+
+
+form > input{
+    width: 100%;
+    border:none;
+    border-radius: 10px;
+    padding: 10px;
+    background:var(--main-background-color-conteiner);
+    color: var(--colorFour);
+    font-size: 1rem;
+    outline: none;
+    border: solid var(--colorSix) 1px;
+}
+
+form > label{
+    color: var(--colorFour);
+    margin: 10px 0 10px 0;
+    width: 100%;
+    font-size: 1.5rem;
+    color: var(--main-background-color-conteiner);
+}
+
+form button{
+    width: 50%;
+    padding: 16px;
+    margin: 25px;
+    border: none;
+    border-radius: 8px;
+    display: block;
+    outline: none;
+    text-transform: uppercase;
+    font-weight: var(--bold-weight);
+    letter-spacing: 4px;
+    color:var(--colorFour);
+    background: var(--colorThree);
+    cursor: pointer;
+}
+
+form select{
+    width: 100%;
+    border:none;
+    border-radius: 5px;
+    padding: 5px;
+    background:var(--main-background-color-conteiner);
+    color: var(--colorFour);
+    font-size: 1rem;
+    outline: none;
+    border: solid var(--colorSix) 1px;
+}
+
+/* Mobile */
+@media only screen and (max-width: 600px){
+    #conteiner form{
+        width: 90%;
+    }
+
+    form h1{
+       font-size: 2.5rem;
+    }
+
+
+    form > label{
+       font-size: 1rem;
+    }
+
+    form button{
+        width: 100%;
+    }
+}
+</style>
+
+<script setup>
+    useHead({
+    title: 'Biblioteca | Cadastro',
+    })
+</script>
---
+++ biblioteca/pages/index.vue
@@ -0,0 +1,201 @@
+<template>
+
+    <div class="main-login">
+        
+        <div class="explicao">
+
+                <h1>Projeto biblioteca</h1>
+                <h2>Uma aplicação web para gerenciar acervo de livros de uma biblioteca e tambem emprestimo de livros feito pelo os usuarios.</h2>
+                <h2><NuxtLink to="/cadastro">Crie seu cadastro aqui</NuxtLink></h2>
+    
+                <img src="img/meninoLendo.png" alt="Menindo lendo um livro em cima da lua">
+        </div>
+
+        <div class="fomularioLogin">
+
+            <div class="card_login">
+
+                <h1>Login</h1>
+
+         
+                <form action="" method="post">
+                                  
+                    <div class="text_field">   
+              
+                        <label for="email">Email</label>
+                        <input type="email" name="email" id="email">
+    
+                        <label for="senha">Senha</label>
+                        <input type="password" name="senha" id="senha">
+
+                        
+                        <button class="butaoLogin" >Login</button>
+                    </div>
+
+                    
+                </form>  
+
+            </div>
+            
+        </div>
+
+    </div>
+
+</template>
+
+
+<style>
+
+* {
+    box-sizing: border-box;
+    padding: 0;
+    margin: 0;
+    font-family: var(--main-font);
+    font-weight: var(--regular-weight);
+}
+
+.main-login{
+   width: 100vw;
+   height: 100vh;
+   background: var(--colorOne);
+   display: flex;
+   justify-content: center;
+   align-items: center; 
+}
+
+.explicao{
+  width: 50vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+}
+
+.explicao h1{
+    color: var(--colorThree);
+    text-align: center;
+}
+
+.explicao h2{
+    text-align:justify;
+    margin: 1rem;
+    color: var(--main-background-color-conteiner);
+}
+
+.fomularioLogin{
+    width: 50vw;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.card_login{
+    width: 50%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    padding: 30px 35px;
+    background:var(--main-background-color-conteiner);
+    border-radius: 20px;
+    box-shadow: 0px 10px 40px var(--colorFour);
+}
+
+.card_login h1{
+    color:var(--colorThree);
+    font-weight: var(--bold-weight);
+    margin: 0;
+}
+
+
+.text_field{
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+}
+
+.text_field > input{
+    width: 100%;
+    border:none;
+    border-radius: 10px;
+    padding: 15px;
+    background:var(--main-background-color-conteiner);
+    color: var(--colorFour);
+    font-size: 12pt;
+    outline: none;
+    border: solid var(--colorSix) 1px;
+}
+
+.text_field > label{
+    color: var(--colorFour);
+    margin: 10px 0 10px 0;
+    width: 100%;
+}
+
+
+.butaoLogin{
+    width: 100%;
+    padding: 16px;
+    margin: 25px;
+    border: none;
+    border-radius: 8px;
+    display: block;
+    outline: none;
+    text-transform: uppercase;
+    font-weight: var(--bold-weight);
+    letter-spacing: 4px;
+    color:var(--colorFour);
+    background: var(--colorThree);
+    cursor: pointer;
+}
+
+
+img{
+    border-radius: 40px;
+    border: solid var(--colorThree) 5px;
+    width: 20vw;
+}
+
+
+@media only screen and (max-width: 950px){
+    .card_login{
+        width: 85%;
+    }
+}
+
+@media only screen and (max-width: 600px){
+    .main-login{
+        flex-direction: column;
+    }
+
+    .explicao{
+        width:100%;
+        height: auto;
+    }
+
+    .fomularioLogin{
+        width: 100%;
+        height: auto;
+    }
+
+    img{
+        display: none;
+    }
+
+    .card_login{
+        padding: 15px 20px;
+    }
+
+}
+
+</style>
+
+<script setup>
+    useHead({
+    title: 'Biblioteca | Login',
+    })
+</script>
