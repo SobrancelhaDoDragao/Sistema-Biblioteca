269d928b4abf9609fc5a0648cbdbc651de399e7c
Eudson <eudson.duraes@gmail.com>
2023-05-04 09:34:15 -0300

    Criando barra de carregamento de página

--- biblioteca/layouts/default.vue
+++ biblioteca/layouts/default.vue
@@ -1,15 +1,14 @@
 <template>
-  <div class="container">
-    
+      <div class="container">
+        <NuxtLoadingBar/>
         <TheHeader></TheHeader>
-
         <div id="conteinerMain">
             <slot />
         </div>
         
         <footer> <a href="https://github.com/SobrancelhaDoDragao" target="_blank">Desenvolvido por Eudson Durães © 2023</a><a href="https://github.com/SobrancelhaDoDragao/Sistema-Biblioteca" target="_blank" class="FooterLinkOculto" >Github</a> <a href="https://www.linkedin.com/in/eudsonduraes/" target="_blank" class="FooterLinkOculto" >Linkedin</a></footer>
         
-        </div>
+      </div>
 
 </template>
 
--- biblioteca/pages/cadastro.vue
+++ biblioteca/pages/cadastro.vue
@@ -1,5 +1,5 @@
 <template>
-
+   <NuxtLoadingBar/>
     <div id="conteiner">
 
   
