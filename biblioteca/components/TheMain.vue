7cdb72a56403b33985619793e1213e0336a158a1
Eudson <eudson.duraes@gmail.com>
2023-05-15 16:11:30 -0300

    Placeholder fica pra depois

--- biblioteca/components/TheMain.vue
+++ biblioteca/components/TheMain.vue
@@ -9,7 +9,7 @@
 
                       <NuxtLink :to="'/auth/acervo/livro/'+livro.id">
 
-                       <nuxt-img class='sobre-livros' :src="livro.capa" sizes="sm:30vw md:15vw lg:8vw"/>
+                       <nuxt-img class='sobre-livros' :src="livro.capa" format="png" sizes="sm:30vw md:15vw lg:8vw"/>
                      
                       </NuxtLink>
                   </div>
@@ -22,7 +22,7 @@
                   <!-- Mudar nome das varoaveis -->
                   <div v-for="livro in livros.novoslivros" :key="livro.id">
                     <NuxtLink :to="'/auth/acervo/livro/'+livro.id">
-                       <nuxt-img class='sobre-livros' :src="livro.capa" format="webp" placeholder sizes="sm:30vw md:15vw lg:8vw"/>
+                       <nuxt-img class='sobre-livros' :src="livro.capa" format="png" sizes="sm:30vw md:15vw lg:8vw"/>
                     </NuxtLink>
                   </div>
 
